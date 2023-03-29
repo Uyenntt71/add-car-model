f6330ecbf5e2cb4cf60a74ce3b17cd995bcdc8b2
Uyenntt71 <uyenntt71@viettel.com.vn>
2023-03-29 20:01:14 +0700

    add crop img

--- add_car_model.py
+++ add_car_model.py
@@ -83,9 +83,28 @@ for r, d, f in os.walk(input_img_dir):
             print("width={}, height={}, depth={}".format(w, h, d))
             count = count + 1
             
+            #crop img
+            imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            ret, thresh = cv2.threshold(imgray, 1, 255, 0)
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            for c in contours:
+                rect = cv2.boundingRect(c)
+                if rect[2] < 100 or rect[3] < 100:
+                    continue
+                else:
+                    break
+            x, y, w, h = rect
+
+            cv2.rectangle(img, (rect[0], rect[1]),  (rect[0] + rect[2], rect[1] + rect[3]),  (0,255,0), 3)
+            img = img[y:y+h, x:x+w]
+            # (h, w, d) = img.shape
+            # print("width={}, height={}, depth={}".format(w, h, d))
+ 
+            
+            
             #resize img
-            ratio = 150 / w 
-            dim = (150, int(h * ratio))
+            ratio = 0.5 
+            dim = (int(w * ratio), int(h * ratio))
             resized = cv2.resize(img, dim)
             (h1, w1, d1) = resized.shape
             print("width={}, height={}, depth={}".format(w1, h1, d1))
